2ab9b602d38e2129c17b8a2dbae2d77716d2cc06
Ndhossain <hhhssnahiid@gmail.com>
2022-11-02 20:12:11 +0600

    Node Mongodb server setup compeleted

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const Port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cj5piaf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }).connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+app.get('/', (req, res) => {
+    res.send("Server is running");
+})
+
+app.listen(Port, () => {
+    console.log(`Server is running on port: ${Port}`);
+})
